87ab1bdb4b2f8fef231f17e116dccd3ea842d734
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-17 13:15:24 +0530

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -51,8 +51,39 @@ Note:
     The given board size is always 9x9.
 
 ______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int r[9][9];
+        int c[9][9];
+        int sm[3][3][9];
+        
+        memset(r,0,sizeof(r));
+        memset(c,0,sizeof(c));
+        memset(sm,0,sizeof(sm));
+        
+        for(int i=0;i<9;i++)
+            for(int j=0;j<9;j++)
+               if(board[i][j] != '.')
+               {
+                   int number = (board[i][j]-'0')-1;
+                   
+                   if(r[i][number])
+                       return false;
+                   if(c[j][number])
+                       return false;
+                   if(sm[i/3][j/3][number])
+                       return false;
+                   
+                   r[i][number]++;
+                   c[j][number]++;
+                   sm[i/3][j/3][number]++;
+               }
+        
+        return true;
+    }
+};
+_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 class Solution {
 public:
     int isSafe(vector<vector<char> > v, int r,int c)
