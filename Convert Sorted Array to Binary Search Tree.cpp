5753102524ff40690b744407c3b4b97a323c4ad4
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-20 18:13:29 +0530

    Create Convert Sorted Array to Binary Search Tree.cpp

---
+++ Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ _______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+ 
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *solve(vector <int> A , int start , int end)
+    {
+        if(start>end)
+            return NULL;
+
+        int mid = start + (end-start)/2;
+        TreeNode *root = new TreeNode(A[mid]);
+        root->left = solve(A,start,mid-1);
+        root->right = solve(A , mid+1, end);
+        return root;
+        
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return solve(nums , 0, nums.size()-1);
+    }
+};
