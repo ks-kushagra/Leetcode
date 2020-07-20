8c5a12c8825b2d9e9193896c325ca8df4c3230a9
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-20 06:30:31 +0530

    Create Maximum Depth of Binary Tree.cpp

---
+++ Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,44 @@
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+____________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int maxDepth(TreeNode* root) {
+        
+        if(root==NULL)
+            return 0;
+        
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        
+        return max(left , right)+1;
+    }
+};
