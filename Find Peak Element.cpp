acf784a79e06effa98fe1e5b2f4343442f8b2dd7
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-26 04:31:30 +0530

    Create Find Peak Element.cpp

---
+++ Find Peak Element.cpp
@@ -0,0 +1,67 @@
+A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 1 or 5 
+Explanation: Your function can return either index number 1 where the peak element is 2, 
+             or index number 5 where the peak element is 6.
+Follow up: Your solution should be in logarithmic complexity.
+
+___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1)
+            return 0; 
+        int start =0;
+        int end = n-1;
+        while(start <= end)
+        {
+            int mid = (start+ (end - start)/2 );
+            
+            if(mid>0 && mid < n-1 )
+            {
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+                return mid;
+            
+            if(nums[mid-1]>nums[mid])
+                end = mid-1;
+            else
+                start = mid+1;
+            }
+            
+            else if(mid == 0)
+            {
+                if(nums[0]>nums[1])
+                    return 0;
+                else
+                    return 1;
+            }
+            
+            else
+            {
+                if(nums[n-1]>nums[n-2])
+                    return n-1;
+                else
+                return n-2;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
