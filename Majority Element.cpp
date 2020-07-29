8ae9af6246c5d02a7535d6d017a682c1a3c98b6e
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-29 12:58:50 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,29 @@
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+_______________________________________________________________________________________________________________________________________________________________________________________________________________________-
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       unordered_map<int,int>  mp;
+       
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        for(auto it = mp.begin();it!=mp.end();it++)
+            if((it->second) > (nums.size()/2))
+                return it->first;
+        
+        return -1;
+            
+    }
+};
