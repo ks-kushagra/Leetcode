69a629ef06f593132695d4093653ddb88e140f1a
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-10-03 11:14:42 +0530

    Create Minimum Depth of Binary Tree.cpp

---
+++ Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,77 @@
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its minimum depth = 2.
+
+_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________-
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root== NULL)
+            return 0;
+        if(root->left == NULL && root->right == NULL)
+            return 1;
+        
+        int left = minDepth(root->left)+1;
+        int right = minDepth(root->right)+1;
+        
+        if(root->left != NULL && root->right == NULL)
+            return left;
+        if(root->right != NULL && root->left == NULL)
+            return right;
+        return min(left , right);
+    }
+};/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root== NULL)
+            return 0;
+        if(root->left == NULL && root->right == NULL)
+            return 1;
+        
+        int left = minDepth(root->left)+1;
+        int right = minDepth(root->right)+1;
+        
+        if(root->left != NULL && root->right == NULL)
+            return left;
+        if(root->right != NULL && root->left == NULL)
+            return right;
+        return min(left , right);
+    }
+};
